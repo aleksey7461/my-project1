a4d3dd0f05bf778bd15949db85ab735bdecc5225
Aleksey_Zavadskiy <aleksey.zavadskiy@gmail.com>
2023-12-21 18:00:30 +0300

    my new commit 1

---
+++ snake_game_test_folder.cpp/test_cls.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <cstdlib>
+#include <conio.h>
+
+int a;
+
+using namespace std;
+int main() { 
+    cout << "qqqqqqqqqqqqqqqqqq";
+    system("cls");
+    //clrscr();
+    cout << "-------"; 
+    return 0; 
+}
