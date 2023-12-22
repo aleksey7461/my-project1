b1590e703d707eb6d519660451f184272ddb2493
Aleksey Zavadskiy <aleksey.zavadskiy@gmail.com>
2023-12-22 13:02:14 +0300

    get & set

--- source.cpp
+++ source.cpp
@@ -4,18 +4,36 @@
 
     class point {
         private:
-        int x;
-        protected:
-        int y;
-        int z;
+
+        int x, y, z;
+
         public:
+
+        int getx() {
+            return x;
+        }
+
+        void setx(int x1) {
+            x = x1;
+        }
+
+        void sety(int y1) {
+            y = y1;
+        }
+
         void print() {
-            cout << "x = " << x << " y = " << y << " z = " << z << "\n";
+            cout << "x = " << x << "\ty = " << y << "\n \n";
         }
     };
 
     int main() {
         point a;
-        a.print();
+        a.sety(15);
+        a.setx(10);
+        
+        int res = a.getx();
+
+        cout << res;
+
         return 0;
     }
