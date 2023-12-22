4c195195abd7d712a2a924f742d5ab8a04994d8b
Aleksey Zavadskiy <aleksey.zavadskiy@gmail.com>
2023-12-22 12:25:48 +0300

    argc & argv

--- source.cpp
+++ source.cpp
@@ -1,19 +1,10 @@
     #include <iostream>
-    
-    #define debug
 
     using namespace std;
 
-    int main() {
-    #ifndef debug
-        cout << "begin\n";
-    #endif 
-        for (int i = 0; i < 5; i++) {
-            cout << i << "\n";
+    int main(int argc, char* argv[]) {
+        for (int i = 0; i < argc; i++) {
+            cout << argv[i] << "\n";
         }
-    #ifndef debug
-        cout << "end\n";
-    #else 
-        cout << "debug able";
-    #endif
+        return 0;
     }
