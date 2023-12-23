dbcba24e284f131ba4fb2dd7f007ac9eae606395
Aleksey Zavadskiy <aleksey.zavadskiy@gmail.com>
2023-12-24 02:08:27 +0300

    string2 & char*

--- string_class.cpp
+++ string_class.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 
 using namespace std;
 
+class mystring {
+private:
+    char *str;
+public:
+    mystring() {
+        str = nullptr;
+    }
+    mystring(const char *str) {
+        int length = strlen(str);
+        cout << length;
+    }
+    ~mystring() {
+
+    }
+};
+
 int main() {
-    
+    mystring str("sex");
     return 0;
 }
