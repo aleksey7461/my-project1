d215f4cdbea760a443c014bd4af6f2b177ad080c
Aleksey Zavadskiy <aleksey.zavadskiy@gmail.com>
2023-12-28 20:54:54 +0300

    virtual inheritance

--- source.cpp
+++ source.cpp
@@ -4,61 +4,41 @@
 
 using namespace std;
 
-class Weapon {
-public:
-    virtual void Shoot() = 0;
+class Character {
+public: 
+    Character() {
+        cout << "Character constructor\n";
+    }
 
-    void Foo() {
-        cout << "foo, huinya\n";
+    int HP;
+};
+
+class Orc : public virtual Character {
+public:
+    Orc() {
+        cout << "Orc constructor\n";
     }
 };
 
-class Gun : public Weapon {
+class Warrior : public virtual Character {
 public:
-    void Shoot() override {
-        cout << "shoot\n";
+    Warrior() {
+        cout << "Warrior constructor\n";
     }
 };
 
-class SubmachineGun : public Gun {
+class OrcWarrior : public Orc, public Warrior {
 public:
-    void Shoot() override {
-        cout << "shoot *3 \n";
-    }
-};
-
-class Boom : public Weapon {
-public:
-    void Shoot() override {
-        cout << "Big Boom\n";
-    }
-};
-
-class Knife : public Weapon {
-public:
-    void Shoot() override {
-        cout << "eb tvou mat' nahui!\n";
-    }
-};
-
-class Player {
-private:
-
-public:
-    void Shoot(Weapon *weapon) {
-        weapon -> Shoot();
+    OrcWarrior() {
+        cout << "OrcWarrior constructor\n";
     }
 };
 
 int main() {
     cout << "\n--------------------\n\n";
 
-    Knife knife;
-    knife.Foo();
-
-    Player a;
-    a.Shoot(&knife);
-
+    OrcWarrior a;
+    
     cout << "\n--------------------\n\n";
     return 0;
 }
